a80e6bcbecafff47d88e96154adc7ff5b96f2ae3
GitHubDoPaulo <pauloeduardosdsilva@gmail.com>
2025-01-16 07:53:57 -0300

    Update conexao.php

--- Tarefa Mister Cheff/model/conexao.php
+++ Tarefa Mister Cheff/model/conexao.php
@@ -1,11 +1,12 @@
 <?php
 function conexao(){
   try {
-    $pdo = new PDO('mysql:host=;dbname=;charset=utf8', '', '');    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO('mysql:host=localhost;dbname=seu_banco_de_dados;charset=utf8', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     return $pdo;
   } catch(PDOException $e) {
-    echo 'Erro DE CONEXÃO: ' . $e->getMessage();
+    echo 'Erro de conexão: ' . $e->getMessage();
   }
 }
 ?>
